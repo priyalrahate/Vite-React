b1dad97f27346e96c80b44cc01c25cf5d28dd5fe
priyalrahate <priyalrahate01@gmail.com>
2025-03-03 18:40:24 +0530

    learning props with configuring tailwind

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from '../components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let someObject={
+    username:"Username",
+    age:21
+  }
+
+  return (
+    <>
+     <h1 className='bg-green-500 text-black p-4 rounded-xl'>Tailwind Test</h1>
+     {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/26922659/pexels-photo-26922659/free-photo-of-train-on-railway-in-countryside.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width="384" height="512"/>
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure> */}
+   {/* <Card username="chaiaurCode" btnText="click me"/> */}
+   <Card username="chaiaurcode" someObj={someObject} />
+   <Card username="hitesh" btnText="click me"/>
+    </>
+  ) 
+}
+
+export default App
